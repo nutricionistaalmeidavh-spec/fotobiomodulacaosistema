a72e060ba1637d7e706063086403dfca230a34d1
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-22 21:44:06 -0300

    feat: expose operational reports through data gateway

--- src/app/public/data/clinical-data-gateway.js
+++ src/app/public/data/clinical-data-gateway.js
@@ -1,11 +1,21 @@
 import { assertAdapterCapabilities, F0_ADAPTER_METHODS, LOCAL_ADAPTER_METHODS } from './contracts.js';
+import { deriveOperationalReport } from './operational-report.js';
 
-export function createClinicalDataGateway({ f0Adapter, localAdapter }) {
+export function createClinicalDataGateway({ f0Adapter, localAdapter, reportAdapter = null }) {
   assertAdapterCapabilities('f0Adapter', f0Adapter, F0_ADAPTER_METHODS);
   assertAdapterCapabilities('localAdapter', localAdapter, LOCAL_ADAPTER_METHODS);
 
   return {
     ...Object.fromEntries(LOCAL_ADAPTER_METHODS.map((name) => [name, (...args) => localAdapter[name](...args)])),
-    ...Object.fromEntries(F0_ADAPTER_METHODS.map((name) => [name, (...args) => f0Adapter[name](...args)]))
+    ...Object.fromEntries(F0_ADAPTER_METHODS.map((name) => [name, (...args) => f0Adapter[name](...args)])),
+    async getOperationalReport(filters = {}) {
+      if (reportAdapter?.getOperationalReport) return reportAdapter.getOperationalReport(filters);
+      const [patients, sessions, protocols] = await Promise.all([
+        localAdapter.listPatients(),
+        f0Adapter.listSessions(),
+        f0Adapter.listProtocols()
+      ]);
+      return deriveOperationalReport({ patients, sessions, protocols, filters });
+    }
   };
 }
